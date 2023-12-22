b55e3f5dc82b68a5c9f3d694231dbe1a2e2b7391
art-me-1011 <krutov.artm@yandex.ru>
2023-12-22 21:57:44 +0300

    score and move camera

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ size = width, height = 700, 1000
 screen = pygame.display.set_mode(size)
 fps = 60
 platform_group = pygame.sprite.Group()
+start_pos = pygame.sprite.Group()
 player = pygame.sprite.Group()
+space = 20
 
 
 def load_image(name, colorkey=None):
@@ -45,6 +47,7 @@ class Doodle(pygame.sprite.Sprite):
         self.image = doodle
         self.speed_y = 0
         self.speed_x = 0
+        self.real_speed_y = 0
         self.pos_x, self.pos_y = pos_x, pos_y
         self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
 
@@ -55,20 +58,41 @@ class Doodle(pygame.sprite.Sprite):
             self.speed_y -= 0.1
         if self.speed_y >= 0:
             self.pos_y -= self.speed_y ** 2
+            self.real_speed_y = self.speed_y ** 2
         else:
             self.pos_y += self.speed_y ** 2
+            self.real_speed_y = -self.speed_y ** 2
         self.rect.y = self.pos_y
 
 
+class Camera:
+    def __init__(self, field_size):
+        self.dx = 0
+        self.dy = 0
+        self.field_size = field_size
+
+    def apply(self, obj, speed_y):
+        obj.rect.y += speed_y
+
+
+class Start_point(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(start_pos)
+        image = load_image('start_point.png', -1)
+        self.rect = image.get_rect().move(pos)
+
 
 if __name__ == '__main__':
+    score = 0
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     running = True
     clock = pygame.time.Clock()
     Common_Plate(309, 700)
-    Common_Plate(109, 700)
+    Common_Plate(109, 500)
     Common_Plate(509, 700)
     player1 = Doodle(320, 630)
+    start_pos1 = Start_point((player1.pos_x, player1.pos_y))
+    camera = Camera((player1.rect.x, player1.rect.y))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -77,12 +101,22 @@ if __name__ == '__main__':
         if keys[pygame.K_LEFT]:
             player1.rect.x -= 5
         elif keys[pygame.K_RIGHT]:
-                player1.rect.x += 5
+            player1.rect.x += 5
         if player1.rect.x + player1.rect.width // 2 < 0:
             player1.rect.x = width - player1.rect.width // 2
         elif player1.rect.x - player1.rect.width // 2 > width:
             player1.rect.x = - player1.rect.width // 2
         player1.update_jump()
+        if (player1.speed_y > 0 and player1.pos_y < 300) or player1.pos_y > 900:
+            for sprite in platform_group:
+                camera.apply(sprite, player1.real_speed_y)
+                if sprite.rect.y > height + 50:
+                    sprite.kill()
+            camera.apply(start_pos1, player1.real_speed_y)
+            player1.pos_y += player1.real_speed_y
+        if score < start_pos1.rect.y - player1.rect.y:
+            score = start_pos1.rect.y - player1.rect.y
+            print(score)
         screen.blit(fon, (0, 0))
         platform_group.draw(screen)
         player.draw(screen)
